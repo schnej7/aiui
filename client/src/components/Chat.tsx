3304fca429b0a8f08a89a45d44b6359423eeefa1
Jerry Schneider <schnej7@gmail.com>
2025-06-17 22:11:01 -0400

    Working conversations

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,9 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+type Role = "assistant" | "user";
+
+type Message = {
+  content: string;
+  role: Role;
+};
 
 function Chat() {
-
+  const [isStreamingResponse, setIsStreamingResponse] = useState(false);
   const [inputText, setInputText] = useState('');
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [activeResponse, setActiveResponse] = useState('');
+
+  useEffect(() => {
+    if (!isStreamingResponse) {
+      setMessages([...messages, {
+        content: activeResponse,
+        role: "assistant",
+      }]);
+      setActiveResponse("");
+    }
+  }, [isStreamingResponse, activeResponse]);
 
   function handleUpdateInputText(event) {
     if (event?.target?.value) {
@@ -19,10 +37,49 @@ function Chat() {
   }
 
   function handleSubmit() {
-    setTimeout(() => {
-      setMessages([...messages, inputText.trim()]);
-      setInputText('');
+    const sanitizedInputText = inputText.trim();
+    setMessages([...messages, {
+      content: `${sanitizedInputText}`,
+      role: "user",
+    }]);
+    console.log("c", messages);
+    sendPrompt(sanitizedInputText);
+    setInputText('');
+  }
+
+  async function sendPrompt(prompt) {
+    if (isStreamingResponse) return;
+
+    setIsStreamingResponse(true);
+    console.log(prompt);
+    const BASE_URL = process.env.REACT_APP_BASE_URL;
+    const LLM_PORT = process.env.REACT_APP_LLM_PORT;
+
+    const response = await fetch(`http://${BASE_URL}:${LLM_PORT}/api/chat`, {
+      method: 'POST',
+      body: JSON.stringify({
+        model: "llama3.1",
+        messages,
+      }),
     });
+
+    console.log(response);
+
+    const decoder = new TextDecoder('utf-8');
+    if (response && response.body) {
+      let newMessage = "";
+      for await (const chunk of response.body) {
+        console.log("d", messages);
+        const decodedChunk = decoder.decode(chunk);
+        const jsonChunk = JSON.parse(decodedChunk);
+
+        newMessage = `${newMessage}${jsonChunk.message.content}`;
+        setActiveResponse(newMessage);
+        console.log(jsonChunk);
+      }
+    }
+
+    setIsStreamingResponse(false);
   }
 
   return (
@@ -31,16 +88,19 @@ function Chat() {
         className="messages"
       >
         {
-          messages.map((message, idx) => (<div key={idx}>{message}</div>))
+          messages.map((message, idx) => (<div key={idx}>{message.content}</div>))
         }
+        <div>{activeResponse}</div>
       </div>
       <textarea
         value={inputText}
         onChange={handleUpdateInputText}
         onKeyDown={handleInputKeyDown}
+        disabled={isStreamingResponse}
       />
       <button
         onClick={handleSubmit}
+        disabled={isStreamingResponse}
       >
         Submit
       </button>
